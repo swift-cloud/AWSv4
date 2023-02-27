ebad26e5cc5716c3b2017855a93fcc7c6b495ff1
Andrew Barba <barba@hey.com>
2023-02-27 17:56:23 -0500

    Update doc

--- Sources/AWSv4/RequestSigner.swift
+++ Sources/AWSv4/RequestSigner.swift
@@ -1,16 +1,12 @@
-//===----------------------------------------------------------------------===//
 //
-// This source file is part of the Soto for AWS open source project
+//  RequestSigner.swift
 //
-// Copyright (c) 2017-2022 the Soto project authors
-// Licensed under Apache License v2.0
 //
-// See LICENSE.txt for license information
-// See CONTRIBUTORS.txt for the list of Soto project authors
+//  Modified by Andrew Barba on 2/27/23.
 //
-// SPDX-License-Identifier: Apache-2.0
+//  Created by Soto Project:
+//  https://github.com/soto-project/soto-core/blob/main/Sources/SotoSignerV4/signer.swift
 //
-//===----------------------------------------------------------------------===//
 
 import Compute
 
